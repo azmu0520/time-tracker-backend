2d546e5461b369940ce7e18f849bd01d395deb16
azmu0520 <azizbekbootcam@gmail.com>
2022-04-19 11:51:22 +0500

    done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Root from './root';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import { BrowserRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
